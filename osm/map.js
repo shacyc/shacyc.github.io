6604ba943a694df34d9cbf7055cfea432e823615
shacyc <44559300+shacyc@users.noreply.github.com>
2020-03-21 16:17:50 +0700

    Update map.js

--- osm/map.js
+++ osm/map.js
@@ -345,17 +345,21 @@ function initMap() {
 
     map = L.map("map", { zoomControl: false }).setView(userLocation ? userLocation : center, zoom);
 
-    L.tileLayer(
-        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw", {
-            maxZoom: 18,
-            attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> ' +
-                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                '<a href="https://www.mapbox.com/">Mapbox</a>',
-            id: "mapbox/streets-v11",
-            tileSize: 512,
-            zoomOffset: -1
-        }
-    ).addTo(map);
+    // L.tileLayer(
+    //     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw", {
+    //         maxZoom: 18,
+    //         attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> ' +
+    //             '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    //             '<a href="https://www.mapbox.com/">Mapbox</a>',
+    //         id: "mapbox/streets-v11",
+    //         tileSize: 512,
+    //         zoomOffset: -1
+    //     }
+    // ).addTo(map);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
 
     /** vẽ vị trí user */
     if (userLocation) {
