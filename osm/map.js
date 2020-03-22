594933068b0a70aa8841d2f8958a213b3437d439
shacyc <44559300+shacyc@users.noreply.github.com>
2020-03-22 10:58:49 +0700

    Update map.js

--- osm/map.js
+++ osm/map.js
@@ -359,7 +359,7 @@ function initMap() {
     //     maxBounds: bounds
     // });
 
-    map = L.map("map", {maxBounds: bounds}).setView(userLocation ? userLocation : center, zoom);
+    map = L.map("map", {maxBounds: bounds, minZoom: 9.5}).setView(userLocation ? userLocation : center, zoom);
 
     // L.tileLayer(
     //     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw", {
